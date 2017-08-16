f5cb833ae64ba524608479b2af9d6253f3cf902b
Victor <victor@iMac-de-Capitan.local>
2017-08-16 17:26:40 +0200

    send mail from contact form

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,21 @@
 const functions = require('firebase-functions');
+
 const express = require('express');
+const bodyParser = require('body-parser');
+
+
+const mailModule = require('./private_modules/mail.js');
 
 const app = express();
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-app.get('/timestamp', (request, response) => {
-  response.send(`El momento actual es: ${Date.now()}`);
+app.get('/testOnline', (request, response) => {
+  response.send(`<h3>Test Online</h3><p>El momento actual es: ${Date.now()}</p>`);
 });
 
+// POST http://localhost:8080/users
+// parameters sent with 
+app.post('/blogcontact', mailModule.sendContactMail);
 
 exports.webapp = functions.https.onRequest(app);
---
+++ functions/private_modules/mail.js
@@ -0,0 +1,36 @@
+const mailModule = {};
+
+const nodemailer = require('nodemailer');
+
+mailModule.sendContactMail = function(req, res){
+  
+  try {
+    const name = req.body.name;
+    const mailAddress = req.body.mailAddress;
+    const message = req.body.message;
+
+    const transporter = nodemailer.createTransport({
+      sendmail: true,
+      newline: 'unix',
+      path: '/usr/sbin/sendmail'
+    });
+    transporter.sendMail({
+        from: `${name}<${mailAddress}>`,
+        to: 'victor.deandres@gmail.com',
+        subject: 'Mensaje desde formulario contacto',
+        text: `${message}`
+    }, (err, info) => {
+        if (err){
+          res.status(202).send(`${err}`);
+        } else {
+          res.status(200).send(`Message from ${name}<${mailAddress}> ${message}`);
+        }
+    });
+  }
+  catch(err) {
+    res.status(202).send(`${err}`);
+  }
+
+};
+
+module.exports = mailModule;
